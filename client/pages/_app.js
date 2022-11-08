2ddda8bc970e7ce4ffe3ef845f432f850b86f877
freijea <jsfreitas_jean@hotmail.com>
2022-11-08 08:39:26 -0300

    server side rendering

---
+++ client/pages/_app.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Custom = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default Custom;
---
+++ client/pages/auth/signup.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await axios.post('/api/users/signup', {
+      email, password
+    });
+
+    console.log(response.data);
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+    <h1>Sign Up</h1>
+    <div className="form-group">
+      <label>Email Address</label>
+      <input 
+        className="form-control" 
+        value={email} 
+        onChange={e => setEmail(e.target.value)}
+      />
+    </div>
+    <div className="form-group">
+      <label>Password</label>
+      <input 
+        className="form-control" 
+        type="password" 
+        value={password} 
+        onChange={e => setPassword(e.target.value)}
+      />
+    </div>
+    <button className="btn btn-primary">Sign Up</button>
+  </form>
+  );
+};
+
+export default SignUp;

e9032644f46e8f247ea30ef920f6ab5de85428b2
freijea <jsfreitas_jean@hotmail.com>
2022-11-08 21:58:21 -0300

    error handling

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,43 +1,49 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
-const SignUp = () => {
+const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password
+    }
+  });
 
-  const onSubmit = async (event) => {
+  const onSubmit = async event => {
     event.preventDefault();
 
-    const response = await axios.post('/api/users/signup', {
-      email, password
-    });
-
-    console.log(response.data);
-  }
+    await doRequest();
+  };
 
   return (
     <form onSubmit={onSubmit}>
-    <h1>Sign Up</h1>
-    <div className="form-group">
-      <label>Email Address</label>
-      <input 
-        className="form-control" 
-        value={email} 
-        onChange={e => setEmail(e.target.value)}
-      />
-    </div>
-    <div className="form-group">
-      <label>Password</label>
-      <input 
-        className="form-control" 
-        type="password" 
-        value={password} 
-        onChange={e => setPassword(e.target.value)}
-      />
-    </div>
-    <button className="btn btn-primary">Sign Up</button>
-  </form>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
   );
 };
 
-export default SignUp;
+export default Signup;

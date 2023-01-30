cdb20c0a8db8b295179e4baa8562e3351f61cd57
freijea <jsfreitas_jean@hotmail.com>
2023-01-30 08:04:46 -0300

    payment service setup

---
+++ payments/src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from 'cookie-session';
+
+import { errorHandler } from '@sitehub-website/common/build/';
+import { NotFoundError } from '@sitehub-website/common/build/';
+import { currentUser } from "@sitehub-website/common/build/";
+
+const app = express();
+app.use(json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    httpOnly: false
+  })
+);
+
+app.use(currentUser);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/index.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  try {
+
+    if(!process.env.JWT_KEY) {
+      throw new Error('environment variable is not defined');
+    }
+
+    if(!process.env.MONGO_URI) {
+      throw new Error ('environment variable is not defined');
+    }
+
+    if(!process.env.NATS_CLIENT_ID) {
+      throw new Error ('environment variable is not defined');
+    }
+
+    if(!process.env.NATS_URL) {
+      throw new Error ('environment variable is not defined');
+    }
+
+    if(!process.env.NATS_CLUSTER_ID) {
+      throw new Error ('environment variable is not defined');
+    }
+
+
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDb");
+
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, ()=>{
+    console.log("Listening on port 3000.");
+  })
+
+};
+
+start();

a6147a7e15782f4a79e06888f74b058c8fef7850
freijea <jsfreitas_jean@hotmail.com>
2023-02-11 11:47:48 -0300

    test deployment workflows

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,7 @@ import { OrderCancelledListener } from "./events/listeners/order-cancelled-liste
 import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 const start = async () => {
+  console.log('Starting up ....');
   try {
 
     if(!process.env.JWT_KEY) {
